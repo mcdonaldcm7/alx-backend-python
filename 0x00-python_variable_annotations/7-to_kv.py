6fa2cf2df4294d24b7b519d33bb214ab720c45d2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-12 00:01:40 +0100

    A type-annotated function that returns a tuple of the string and integer OR float argument passed

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pyhton3
+"""
+Task
+7. Complex types - string and int/float to tuple
+
+Write a type-annotated function 'to_kv' that takes a string 'k' and an int OR
+float 'v' as arguments and returns a tuple. The first element of the tuple is
+the string 'k'. The second element is the square of the int/float 'v' and
+should be annotated as a float.
+"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+    """
+    Return: A tuple with first key k and second key v ** 2
+    """
+    return (k, v**2)
