845c01fb4839bae32d2e606c34b6097b8e8b31fe
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-11 23:45:51 +0100

    A simple type-annotated function to return a floored float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task
+2. Basic annotations - floor
+
+Write a type-annotated function 'floor' which takes a float 'n' as argument and
+returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Return: floored n
+    """
+    return math.floor(n)
