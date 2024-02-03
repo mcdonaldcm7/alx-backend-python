22b32dff7766d2feae985584a8725160d37d47ef
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-03 15:42:50 +0100

    Added unittest for 'access_nestedmap_exception'

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,14 +27,27 @@ class TestAccessNestedMap(unittest.TestCase):
     Subclass of unittest class to test the `access_nested_map` function
     """
     @parameterized.expand([
-        ({"a": 1}, "a", 1),
-        ({"a": {"b": 2}}, "a", {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence,
                                exp_result: Any):
         """
-        Test for the `access_nested_map` method
+        Test for the `access_nested_map` return value for valid inputs
         """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, exp_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+        ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence, exp_result: Any):
+        """
+        Test for the Exception the 'access_nested_map' function raises
+        """
+        kwargs = {"nested_map": nested_map, "path": path}
+        self.assertRaises(KeyError, access_nested_map, kwargs,
+                          "{}".format(path[len(path) - 1]))
