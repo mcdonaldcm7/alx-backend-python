bfce0fdcda5cfd6169bc3964ca3757d6eaf824a2
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-05 14:56:33 +0100

    Renamed Arguments

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -92,10 +92,9 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
         ])
-    def test_has_license(self, license: Dict, license_key: str,
-                         has_license: bool):
+    def test_has_license(self, repo: Dict, license_key: str, has_license: bool):
         """
         Unittest for GithubOrgClient static method `has_license`
         """
-        result_has_license = GithubOrgClient.has_license(license, license_key)
+        result_has_license = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result_has_license, has_license)
